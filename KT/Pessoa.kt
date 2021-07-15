d90e1e702d8f6a26410665b0a3c6334be2768489
RodrigoSester <rodrigows09@gmail.com>
2021-07-15 03:32:57 -0300

    kotlin codes

---
+++ KT/Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa {
+    var nome: String = "Rodrigo"
+    var cpf: String = "860.212.220-91"
+}
+
+fun main() {
+    val rodrigo = Pessoa()
+
+    println(rodrigo.nome)
+    println(rodrigo.cpf)
+}
