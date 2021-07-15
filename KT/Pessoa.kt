b03e9e1afbbf4d838d1fa6c247eba03a2a004d91
RodrigoSester <rodrigows09@gmail.com>
2021-07-15 04:13:33 -0300

    update to simplify the print

--- KT/Pessoa.kt
+++ KT/Pessoa.kt
@@ -4,16 +4,18 @@ class Pessoa {
     var cpf: String = "860.212.220-91"
     private set // Assim apenas seto a váriavel dentro da class, sem poder alterar ela fora, apenas fazer o get
 
-    inner class Endereco {
+    inner class Endereco { // class dentro da class
         var rua: String = "Av. Guilherme Schell"
     }
+
+    constructor()
+
+    fun pessoaInfo() = "$nome \n$cpf"
 }
 
 fun main() {
     val rodrigo = Pessoa()
-
-    println(rodrigo.nome)
-    println(rodrigo.cpf)
+    println(rodrigo.pessoaInfo())
 
     println(rodrigo.Endereco().rua)
 }
