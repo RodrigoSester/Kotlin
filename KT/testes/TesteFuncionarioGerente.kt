5e4bfd6d91fbeb710e1d01874e5a887e20f147ca
RodrigoSester <rodrigows09@gmail.com>
2021-07-20 00:44:26 -0300

    updated to authenticate and print report

--- KT/testes/TesteFuncionarioGerente.kt
+++ KT/testes/TesteFuncionarioGerente.kt
@@ -1,9 +1,11 @@
 package KT.testes
 
-import Git.KT.Funcionario
 import KT.Gerente
 
 fun main(){
     val maria = Gerente(nome = "Maria", cpf = "43456", salario = 5000.0, senha = "senha")
+
     ImprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
