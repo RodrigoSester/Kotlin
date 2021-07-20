fc7d513bc0ea1b0a121cd699fc9f9f6593cadc0f
RodrigoSester <rodrigows09@gmail.com>
2021-07-20 00:31:40 -0300

    rename tests

--- KT/testes/TesteFuncionarioGerente.kt
+++ KT/testes/TesteFuncionarioGerente.kt
@@ -4,6 +4,6 @@ import Git.KT.Funcionario
 import KT.Gerente
 
 fun main(){
-    val maria = Gerente(nome = "Maria", cpf = "43456", salario = 5000.0)
+    val maria = Gerente(nome = "Maria", cpf = "43456", salario = 5000.0, senha = "senha")
     ImprimeRelatorioFuncionario.imprime(maria)
 }
